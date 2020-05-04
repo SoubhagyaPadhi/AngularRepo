e8000390cd1011826bac0e39de015f8645e4d34d
SoubhagyaPadhi <soubhagya.padhi49@gmail.com>
2020-05-04 18:20:45 +0530

    Update with Evet Binding

--- PropertyBindingTest/src/app/even-bind/even-bind.component.ts
+++ PropertyBindingTest/src/app/even-bind/even-bind.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-even-bind',
   template: `<button (click)=onClick()>Click Me</button>
-  <h2 [style.color]="'red'">{{greetMsg}}</h2>
+  <h2 [ngStyle]="setStyle">{{greetMsg}}</h2>
   <button (click)=onClickEvent($event)>Event Click</button><br/><br/>
   <button (click)="greetMsg = 'Welcome Button Click!!'">Inline Click</button>`,
   styleUrls: ['./even-bind.component.css']
@@ -12,6 +12,10 @@ export class EvenBindComponent implements OnInit {
 
   public greetMsg = "";
   public eventType = "";
+  public setStyle = {
+    color:"red",
+    fontStyle:"italic"
+  };
   constructor() { }
 
   ngOnInit() {
--- PropertyBindingTest/src/app/temp-ref-var/temp-ref-var.component.ts
+++ PropertyBindingTest/src/app/temp-ref-var/temp-ref-var.component.ts
@@ -3,17 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-temp-ref-var',
   template: `<input #myInput type="text">
+             <h2 [ngClass]="setStyleClass">{{message}}</h2>
              <button (click)=logMsg(myInput.value)>Click Me</button>
              <button (click)=logMsg(myInput)>Click</button>`,
-  styleUrls: ['./temp-ref-var.component.css']
+  styles: [`.txt-danger{
+    color:red;
+    font-style:italic
+  }
+  .txt-special{
+    color:blue;
+    font-style:bold;
+  }`]
 })
 export class TempRefVarComponent implements OnInit {
 
+  public message = "";
+  public isDanger = false;
+  public setStyleClass = {
+   "txt-danger":this.isDanger,
+   "txt-special":!this.isDanger
+  };
   constructor() { }
 
   ngOnInit() {
   }
   logMsg(val) {
+    this.message = val;
     console.log(val);
   }
 
